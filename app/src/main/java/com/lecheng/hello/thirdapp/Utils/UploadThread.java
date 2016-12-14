687edd3f539da52862768c83278d21ad160c4f73
Cheng <lechenggood@foxmail.com>
2016-12-14 22:55:38 +0800

    this 2016-12-14 22:55

--- app/src/main/java/com/lecheng/hello/thirdapp/Utils/UploadThread.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/Utils/UploadThread.java
@@ -39,86 +39,86 @@ public class UploadThread extends Thread {
 
     @Override
     public void run() {
-        uploadFile();
+//        uploadFile();
+        yuanshengshangchuan();
     }
 
     private void uploadFile() {
-        HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(url);
-        MultipartEntity multi = new MultipartEntity();
-
-        File fileAbs = new File(Environment.getExternalStorageDirectory(), "bc.jpg");
+//        HttpClient client = new DefaultHttpClient();
+//        HttpPost post = new HttpPost(url);
+//        MultipartEntity multi = new MultipartEntity();
+//
 //        File fileAbs = new File(Environment.getExternalStorageDirectory(), "bc.jpg");
-        FileBody fileBody = new FileBody(fileAbs);
-
-        multi.addPart("file", fileBody);
-        post.setEntity(multi);
-
-        try {
-            HttpResponse response = client.execute(post);
-            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                System.out.println(EntityUtils.toString(response.getEntity()));
-//                Toast.makeText(c, "上传成功：" + EntityUtils.toString(response.getEntity()), Toast.LENGTH_LONG).show();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+////        File fileAbs = new File(Environment.getExternalStorageDirectory(), "bc.jpg");
+//        FileBody fileBody = new FileBody(fileAbs);
+//
+//        multi.addPart("file", fileBody);
+//        post.setEntity(multi);
+//
+//        try {
+//            HttpResponse response = client.execute(post);
+//            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+//                System.out.println(EntityUtils.toString(response.getEntity()));
+////                Toast.makeText(c, "上传成功：" + EntityUtils.toString(response.getEntity()), Toast.LENGTH_LONG).show();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
     //原生上传文件
     private void yuanshengshangchuan() {
-//        String boundary = "-----------------------------7de2c25201d48";
-//        String prefix = "--";
-//        String end = "\r\n";
-//
-//        try {
-//            URL httpUrl = new URL(url);
-//            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
-//            conn.setRequestMethod("POST");
-//            conn.setDoOutput(true);
-//            conn.setDoInput(true);
-//            conn.setRequestProperty("Content-Type",
-//                    "multipart/form-data;boundary=" + boundary);
-//            DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-//            out.writeBytes(prefix + boundary + end);
-//            out.writeBytes("Content-Disposition: form-data;"
-//                    + "name=\"file\";filename=" + "bc.jpg" + "\"" + end);
-//            out.writeBytes(end);
-//            FileInputStream fileInputStream =
-//                    new FileInputStream(new File(fileName));
-//            byte[] b = new byte[1024 * 4];
-//            int len;
-//            while ((len = fileInputStream.read(b)) != -1) {
-//                out.write(b, 0, len);
-//            }
-//            out.writeBytes(end);
-//            out.writeBytes(prefix + boundary + prefix + end);
-//            out.flush();
-//
-//            BufferedReader reader =
-//                    new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//            StringBuffer sb = new StringBuffer();
-//            String str;
-//            while ((str = reader.readLine()) != null) {
-//                sb.append(str);
-//            }
-//
-//            System.out.println("respose" + sb.toString());
-//            if (out != null) {
-//                out.close();
-//            }
-//            if (reader != null) {
-//                reader.close();
-//            }
-//            System.out.println("提交成功");
-//            Toast.makeText(c, "提交成功", Toast.LENGTH_LONG).show();
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//            System.out.println("MalformedURLException:" + e);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.out.println("IOException:" + e);
-//        }
+        String boundary = "---------------------------7de2c25201d48";
+        String prefix = "--";
+        String end = "\r\n";
+
+        try {
+            URL httpUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data;boundary=" + boundary);
+            DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+            out.writeBytes(prefix + boundary + end);
+            out.writeBytes("Content-Disposition: form-data;"
+                    + "name=\"file\";filename=\"" + "bc.jpg" + "\"" + end);
+            out.writeBytes(end);
+            FileInputStream fileInputStream = new FileInputStream(new File(fileName));
+            byte[] b = new byte[1024 * 4];
+            int len;
+            while ((len = fileInputStream.read(b)) != -1) {
+                out.write(b, 0, len);
+            }
+            out.writeBytes(end);
+            out.writeBytes(prefix + boundary + prefix + end);
+            out.flush();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                sb.append(str);
+            }
+
+            System.out.println("respose" + sb.toString());
+            if (out != null) {
+                out.close();
+            }
+            if (reader != null) {
+                reader.close();
+            }
+            System.out.println("提交成功");
+            Toast.makeText(c, "提交成功", Toast.LENGTH_LONG).show();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.out.println("MalformedURLException:" + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException:" + e);
+        }
     }
 }
