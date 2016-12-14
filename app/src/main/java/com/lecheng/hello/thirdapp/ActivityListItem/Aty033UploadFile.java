2adaf8d93b8747ff9be3c23235bd53364ac6c8ca
lecheng <lechenggood@foxmail.com>
2016-12-14 17:20:32 +0800

    12-14 16.25

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityListItem/Aty033UploadFile.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityListItem/Aty033UploadFile.java
@@ -20,6 +20,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
@@ -28,14 +29,97 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lecheng.hello.thirdapp.R;
+import com.lecheng.hello.thirdapp.Utils.UploadThread;
 import com.lidroid.xutils.http.RequestParams;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 
-public class Aty033UploadFile extends Activity {
+public class Aty033UploadFile extends AppCompatActivity {
+    private String newName = "bc.jpg";
+    //要上传的本地文件路径
+    private String uploadFile = Environment.getExternalStorageDirectory() + "bc.jpg";
+    //上传到服务器的指定位置
+    private String actionUrl = "http://tests.tctu.cn/mapi.php/UploadImg/upload";
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aty031_blur);
+        findViewById(R.id.aty32_btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://tests.tctu.cn/mapi.php/UploadImg/upload";
+                File file = Environment.getExternalStorageDirectory();
+                File fielAbs = new File(file, "bc.jpg");
+                String fileName = fielAbs.getAbsolutePath();
+                UploadThread httpThread = new UploadThread(getApplicationContext(), url, fileName);
+                httpThread.start();
+//                new Thread() {
+//                    @Override
+//                    public void run() {
+//                        uploadFile();
+//                    }
+//                }.start();
+
+
+            }
+        });
+    }
+
+    private void uploadFile() {
+        String end = "/r/n";
+        String Hyphens = "--";
+        String boundary = "*****";
+        try {
+            URL url = new URL(actionUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      /* 允许Input、Output，不使用Cache */
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+      /* 设定传送的method=POST */
+            con.setRequestMethod("POST");
+      /* setRequestProperty */
+            con.setRequestProperty("Connection", "Keep-Alive");
+            con.setRequestProperty("Charset", "UTF-8");
+            con.setRequestProperty("Content-Type",
+                    "multipart/form-data;boundary=" + boundary);
+      /* 设定DataOutputStream */
+            DataOutputStream ds = new DataOutputStream(con.getOutputStream());
+            ds.writeBytes(Hyphens + boundary + end);
+            ds.writeBytes("Content-Disposition: form-data;"
+                    + "name=\"file\";filename=" + "bc.jpg" + "\"" + end);
+            ds.writeBytes(end);
+      /* 取得文件的FileInputStream */
+            FileInputStream fStream = new FileInputStream(uploadFile);
+      /* 设定每次写入1024bytes */
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int length = -1;
+      /* 从文件读取数据到缓冲区 */
+            while ((length = fStream.read(buffer)) != -1) {
+        /* 将数据写入DataOutputStream中 */
+                ds.write(buffer, 0, length);
+            }
+            ds.writeBytes(end);
+            ds.writeBytes(Hyphens + boundary + Hyphens + end);
+            fStream.close();
+            ds.flush();
+      /* 取得Response内容 */
+            InputStream is = con.getInputStream();
+            int ch;
+            StringBuffer b = new StringBuffer();
+            while ((ch = is.read()) != -1) {
+                b.append((char) ch);
+            }
+            System.out.println("上传成功");
+            Toast.makeText(getApplicationContext(), "上传成功", Toast.LENGTH_LONG)
+                    .show();
+            ds.close();
+        } catch (Exception e) {
+            System.out.println("上传失败" + e.getMessage());
+            Toast.makeText(getApplicationContext(), "上传失败" + e, Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/lecheng/hello/thirdapp/Utils/UploadThread.java
@@ -0,0 +1,86 @@
+package com.lecheng.hello.thirdapp.Utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Administrator on 2016/12/14.
+ */
+public class UploadThread extends Thread {
+    private String fileName, url;
+    private Context c;
+
+
+    public UploadThread(Context c, String url, String fileName) {
+        this.url = url;
+        this.fileName = fileName;
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        String boundary = "-----------------------------7de2c25201d48";
+        String prefix = "--";
+        String end = "\r\n";
+
+        try {
+            URL httpUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data;boundary=" + boundary);
+            DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+            out.writeBytes(prefix + boundary + end);
+            out.writeBytes("Content-Disposition: form-data;"
+                    + "name=\"file\";filename=" + "bc.jpg" + "\"" + end);
+            out.writeBytes(end);
+            FileInputStream fileInputStream =
+                    new FileInputStream(new File(fileName));
+            byte[] b = new byte[1024 * 4];
+            int len;
+            while ((len = fileInputStream.read(b)) != -1) {
+                out.write(b, 0, len);
+            }
+            out.writeBytes(end);
+            out.writeBytes(prefix + boundary + prefix + end);
+            out.flush();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                sb.append(str);
+            }
+
+            System.out.println("respose" + sb.toString());
+            if (out != null) {
+                out.close();
+            }
+            if (reader != null) {
+                reader.close();
+            }
+            System.out.println("提交成功");
+            Toast.makeText(c, "提交成功", Toast.LENGTH_LONG).show();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.out.println("MalformedURLException:" + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException:" + e);
+        }
+
+    }
+}
