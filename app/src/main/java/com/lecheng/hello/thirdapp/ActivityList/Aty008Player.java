6b5d09dbbe32e915891c5596ae018ce61472a827
Cheng <lechenggood@foxmail.com>
2017-03-04 15:06:44 +0800

    添加了一个可以后台的音乐播放器

---
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty008Player.java
@@ -0,0 +1,39 @@
+package com.lecheng.hello.thirdapp.ActivityList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.lecheng.hello.thirdapp.R;
+import com.lecheng.hello.thirdapp.Service.SerMedia;
+import com.lecheng.hello.thirdapp.Utils.MyToast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Aty008Player extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity008);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.v1, R.id.v2, R.id.v3})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.v1:
+                stopService(new Intent(Aty008Player.this, SerMedia.class));
+                break;
+            case R.id.v2:
+                startService(new Intent(Aty008Player.this, SerMedia.class));
+                break;
+            case R.id.v3:
+                new MyToast(Aty008Player.this, "下一首", 3333);
+                break;
+        }
+    }
+}
