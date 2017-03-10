f70f6fa26e3c39645936c0867e6601dbb0ac095a
公司 <951827860@qq.com>
2017-03-10 10:36:07 +0800

    Aty047 MVC架构研究

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty047MVC.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty047MVC.java
@@ -4,11 +4,14 @@ package com.lecheng.hello.thirdapp.ActivityList;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.lecheng.hello.thirdapp.R;
 import com.lecheng.hello.thirdapp.Utils.MyToast;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.download.ImageDownloader;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,26 +23,12 @@ public class Aty047MVC extends AppCompatActivity implements Aty047MVC_Listener {
     EditText editText3;
     @Bind(R.id.textView10)
     TextView textView10;
-    @Bind(R.id.textView11)
-    TextView textView11;
-    @Bind(R.id.textView12)
-    TextView textView12;
-    @Bind(R.id.textView13)
-    TextView textView13;
-    @Bind(R.id.textView14)
-    TextView textView14;
-    @Bind(R.id.textView15)
-    TextView textView15;
-    @Bind(R.id.textView16)
-    TextView textView16;
-    @Bind(R.id.textView17)
-    TextView textView17;
-    @Bind(R.id.textView18)
-    TextView textView18;
-    @Bind(R.id.textView19)
-    TextView textView19;
-    @Bind(R.id.aty047_mvc)
+
+
     LinearLayout aty047Mvc;
+    @Bind(R.id.image1)
+    ImageView image1;
+    ImageLoader imageLoader = ImageLoader.getInstance();
     private Aty047MVC_ModelImpl weatherModel;
 
     @Override
