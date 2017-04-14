f8ecd6d1578fb359b6f0c8cea72d7acccd170984
公司 <951827860@qq.com>
2017-04-14 11:18:21 +0800

    aty054放弃

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty054.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty054.java
@@ -1,15 +1,43 @@
 package com.lecheng.hello.thirdapp.ActivityList;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.lecheng.hello.thirdapp.R;
-//tagflowlayout
+import com.lecheng.hello.thirdapp.Widgets.flowlayout.FlowLayout;
+import com.lecheng.hello.thirdapp.Widgets.flowlayout.TagAdapter;
+import com.lecheng.hello.thirdapp.Widgets.flowlayout.TagFlowLayout;
+
+import java.util.zip.Inflater;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/*tagflowlayout
+* 教程地址：http://blog.csdn.net/lmj623565791/article/details/48393217*/
 public class Aty054 extends ActionBarActivity {
+
+    @Bind(R.id.tfl)
+    TagFlowLayout tfl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty054);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.btn1, R.id.btn2})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+                break;
+            case R.id.btn2:
+                break;
+        }
     }
 }
