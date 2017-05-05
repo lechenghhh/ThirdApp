b3dd39abb1b97d203bc820423b6a90d538ee33ae
公司 <951827860@qq.com>
2017-05-05 11:03:44 +0800

    pulltorefresh 100%使用

---
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty055View.java
@@ -0,0 +1,15 @@
+package com.lecheng.hello.thirdapp.ActivityList;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.lecheng.hello.thirdapp.R;
+
+public class Aty055View extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty055_view);
+    }
+}
