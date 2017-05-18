73c7b923434845b16e6697d7eb67a97b6e535257
公司 <951827860@qq.com>
2017-05-18 15:21:20 +0800

    aty057 图表

---
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty057Chart.java
@@ -0,0 +1,15 @@
+package com.lecheng.hello.thirdapp.ActivityList;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.lecheng.hello.thirdapp.R;
+
+public class Aty057Chart extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty057_chart);
+    }
+}
