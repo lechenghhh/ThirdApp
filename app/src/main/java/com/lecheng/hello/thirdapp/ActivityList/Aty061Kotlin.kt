6a4d648eaf8eafcc8f757249745c22d8ffac26ae
公司 <951827860@qq.com>
2017-05-19 11:56:41 +0800

    Kotlin 实践》》》2

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty061Kotlin.kt
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty061Kotlin.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import com.lecheng.hello.thirdapp.R
 
 import kotlinx.android.synthetic.main.aty061_kotlin. *              //依赖kotlinx的xml找到控件
-
+//import kotlinx.android.synthetic.main.
 class Aty061Kotlin : ActionBarActivity() {
     private var i = 0;
 
