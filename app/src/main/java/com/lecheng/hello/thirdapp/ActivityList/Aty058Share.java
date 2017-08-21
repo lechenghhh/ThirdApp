a8c9fb8a39a6138c797427c92933dad6466bf7d4
Cheng <lechenggood@foxmail.com>
2017-08-21 22:11:25 +0800

    zengjia图片分享至微信

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty058Share.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty058Share.java
@@ -42,15 +42,19 @@ import static com.lecheng.hello.thirdapp.Utils.MyApplication.getHttpQue;
 
 //多图分享到微信：http://blog.csdn.net/xiazdong/article/details/7687439
 public class Aty058Share extends ActionBarActivity {
-    public static Context mContext;
     private int IMAGE_NAME = 0;
+    ArrayList<String> arrImg = new ArrayList<String>();
+    ArrayList<String> arrShare = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty058_share);
         ButterKnife.bind(this);
-        mContext = this;
+        arrImg.add("http://wx4.sinaimg.cn/mw690/48d84e6fly1firh5odb69j20ps0fvwwk.jpg");
+        arrImg.add("http://fanyi.baidu.com/static/translation/img/header/logo_cbfea26.png");
+        arrImg.add("https://os.alipayobjects.com/rmsportal/uGWcpAFgWdynxBy.png");
+        arrImg.add("https://ss0.baidu.com/73t1bjeh1BF3odCf/it/u=2512058291,4137050375&fm=85&s=5B5E38D210FE598C627CB649030040F5");
     }
 
 
@@ -58,73 +62,65 @@ public class Aty058Share extends ActionBarActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn1:
-//                for (int i = 0; i < lenght; i++) {
-//                    File file = saveImageToSdCard(images.get(i));
-//                    files.add(file);
-//                    F.makeLog(file.toString());
-//                }
-//
-//
-                Intent intent = new Intent();
-                ComponentName comp = new ComponentName("com.tencent.mm",
-                        "com.tencent.mm.ui.tools.ShareToTimeLineUI");
-                intent.setComponent(comp);
-                intent.setAction(Intent.ACTION_SEND_MULTIPLE);
-                intent.setType("image/*");
-                intent.putExtra("Kdescription", "这是我分享的图片");
-                ArrayList<Uri> imageUris = new ArrayList<Uri>();
-//                for (File f : files) {
-//                    imageUris.add(Uri.fromFile(f));
-//                }
-                imageUris.add(Uri.fromFile(
-                        new File("http://avatar.csdn.net/9/2/1/1_u011340932.jpg")));
-//                imageUris.add(Uri.fromFile(
-//                        new File(Environment.getExternalStorageDirectory() + "/timg.jpeg")));
-//                imageUris.add(Uri.fromFile(
-//                        new File(Environment.getExternalStorageDirectory() + "/timg.jpeg")));
-                intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
-                startActivity(intent);
                 break;
             case R.id.btn2:
-                final String url = "http://wx4.sinaimg.cn/mw690/48d84e6fly1firh5odb69j20ps0fvwwk.jpg";
-                ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
-                    @Override//图片保存方法http://blog.csdn.net/guolin_blog/article/details/17482165/
-                    public void onResponse(Bitmap response) {
-//                                imageView.setImageBitmap(response);
-                        System.out.println("Volley-Success");
-                        saveBitmap(response, url);
-                    }
-                }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println("VolleyError error");
-                    }
-                });
-                getHttpQue().add(imageRequest);
+                getImg(arrImg);
                 break;
             case R.id.btn3:
                 System.out.println("Environment:" + Environment.getExternalStorageDirectory());
-//                startActivity(new Intent(this, Aty058_1.class));
                 break;
         }
     }
 
-    public void saveBitmap(Bitmap bm, String picName) {
-
-        System.out.println("保存图片...");
-        File f = new File(Environment.getExternalStorageDirectory() + "/Android/", picName.replace("/", ""));
-        if (f.exists()) f.delete();
-        try {
-            FileOutputStream out = new FileOutputStream(f);
-            bm.compress(Bitmap.CompressFormat.PNG, 90, out);
-            out.flush();
-            out.close();
-            System.out.println("保存成功");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println(e + "");
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void getImg(final ArrayList<String> arrUrl) {
+        for (int i = 0; i < arrUrl.size(); i++) {
+            final int finalI = i;
+            ImageRequest imageRequest = new ImageRequest(arrUrl.get(i), new Response.Listener<Bitmap>() {
+                @Override//图片保存方法http://blog.csdn.net/guolin_blog/article/details/17482165/
+                public void onResponse(Bitmap bm) {
+                    System.out.println("Volley-Success");
+                    File file = new File(Environment.getExternalStorageDirectory() + "/Android/img/", finalI + ".jpg");
+                    File fDir = new File(Environment.getExternalStorageDirectory() + "/Android/img/");
+                    if (!fDir.exists()) fDir.mkdir();
+                    try {
+                        FileOutputStream out = new FileOutputStream(file);
+                        bm.compress(Bitmap.CompressFormat.PNG, 90, out);
+                        out.flush();
+                        out.close();
+                        arrShare.add(Environment.getExternalStorageDirectory() + "/Android/img/" + finalI + ".jpg");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        System.out.println(e + "");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (finalI == arrUrl.size() - 1) share2Wx(arrShare);
+                }
+            }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("VolleyError error");
+                }
+            });
+            getHttpQue().add(imageRequest);
         }
     }
+
+    public void share2Wx(ArrayList<String> arrayList) {
+        Intent intent = new Intent();
+        ComponentName comp = new ComponentName("com.tencent.mm", "com.tencent.mm.ui.tools.ShareToTimeLineUI");
+        intent.setComponent(comp);
+        intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        intent.setType("image/*");
+        intent.putExtra("Kdescription", "这是我分享的图片");
+        ArrayList<Uri> imageUris = new ArrayList<Uri>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println("arrSHare:" + arrayList.get(i));
+            imageUris.add(Uri.fromFile(new File(arrayList.get(i))));
+        }
+        intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
+        startActivity(intent);
+
+    }
+
 }
