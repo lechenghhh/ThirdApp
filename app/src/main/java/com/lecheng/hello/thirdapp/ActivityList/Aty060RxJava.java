ca7e1f33cd196a87ae6477a42cb056767d99d427
Cheng <lechenggood@foxmail.com>
2017-12-19 16:51:19 +0800

    RxAndroid引入

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty060RxJava.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty060RxJava.java
@@ -2,11 +2,14 @@ package com.lecheng.hello.thirdapp.ActivityList;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.lecheng.hello.thirdapp.R;
 import com.lecheng.hello.thirdapp.Utils.MyToast;
+
+import org.reactivestreams.Subscriber;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,12 +18,17 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class Aty060RxJava extends AppCompatActivity {
 
     @Bind(R.id.tvScreen)
     TextView tvScreen;
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,60 +38,89 @@ public class Aty060RxJava extends AppCompatActivity {
 //        myObservable.observeOn(AndroidSchedulers.mainThread()).subscribe(mySubscriber);
     }
 
-//    //被观察者
-//    Observable<String> sender = Observable.create(new Observable.OnSubscribe<String>() {
-//        @Override
-//        public void call(Subscriber<? super String> subscriber) {
-//            subscriber.onNext("Hi，Weavey!");  //发送数据"Hi，Weavey!"
-//        }
-//    });
-//
-//    //观察者
-//    Observer<String> receiver = new Observer<String>() {
-//        @Override
-//        public void onCompleted() {//数据接收完成时调用
-//        }
-//
-//        @Override
-//        public void onError(Throwable e) {//发生错误调用
-//        }
-//
-//        @Override
-//        public void onNext(String s) {//正常接收数据调用
-//            new MyToast(Aty060RxJava.this, s, 1);//将接收到来自sender的问候"Hi，Weavey!
-//            tvScreen.setText(s);
-//        }
-//    };
+    //被观察者
+    Observable<String> sender = Observable.create(new ObservableOnSubscribe<String>() {
+        @Override
+        public void subscribe(ObservableEmitter<String> observableEmitter) throws Exception {
+            observableEmitter.onNext("Hi，Weavey!");  //发送数据"Hi，Weavey!"
+            observableEmitter.onComplete();
+        }
+    });
+
+    //观察者
+    Observer<String> receiver = new Observer<String>() {
+        @Override
+        public void onError(Throwable e) {//发生错误调用
+        }
+
+        @Override
+        public void onComplete() {
+        }
+
+        @Override
+        public void onSubscribe(Disposable disposable) {
+        }
+
+        @Override
+        public void onNext(String s) {//正常接收数据调用
+            new MyToast(Aty060RxJava.this, s, 1);//将接收到来自sender的问候"Hi，Weavey!
+            tvScreen.setText(s);
+        }
+    };
+
+    private void startDownload() {
+        final Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                for (int i = 0; i < 100; i++) {
+                    if (i % 20 == 0) {
+                        try {
+                            Thread.sleep(500); //模拟下载的操作。
+                        } catch (InterruptedException exception) {
+                            if (!e.isDisposed()) {
+                                e.onError(exception);
+                            }
+                        }
+                        e.onNext(i);
+                    }
+                }
+                e.onComplete();
+            }
+
+        });
+        DisposableObserver<Integer> disposableObserver = new DisposableObserver<Integer>() {
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d("DownloadActivity", "onNext=" + value);
+                tvScreen.setText("当前下载进度：" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("DownloadActivity", "onError=" + e);
+                tvScreen.setText("下载失败");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("DownloadActivity", "onComplete");
+                tvScreen.setText("下载成功");
+            }
+        };
+        observable.subscribeOn(Schedulers.single()).observeOn(AndroidSchedulers.mainThread()).subscribe(disposableObserver);
+        mCompositeDisposable.add(disposableObserver);
+    }
 
     @OnClick({R.id.btn1, R.id.btn2, R.id.btn3})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn1:
-//                sender.subscribe(receiver);
+                sender.subscribe(receiver);
                 break;
             case R.id.btn2:
-//                Observable<String> sender2 = Observable.create(new Observable.OnSubscribe<String>() {
-//                    @Override
-//                    public void call(Subscriber<? super String> subscriber) {
-//                        subscriber.onNext("这是第二个事件");
-//                        subscriber.onCompleted();
-//                    }
-//                });
-//                sender2.subscribe(new Observer<String>() {
-//                    @Override
-//                    public void onCompleted() {
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable throwable) {
-//                    }
-//
-//                    @Override
-//                    public void onNext(String s) {
-//                        new MyToast(Aty060RxJava.this, s, 1);
-//                        tvScreen.setText(s);
-//                    }
-//                });
+                startDownload();
                 break;
             case R.id.btn3:
                 Observable<String> sender3 = Observable.create(new ObservableOnSubscribe<String>() {
@@ -106,12 +143,10 @@ public class Aty060RxJava extends AppCompatActivity {
 
                     @Override
                     public void onError(Throwable throwable) {
-
                     }
 
                     @Override
                     public void onComplete() {
-
                     }
                 });
                 break;
