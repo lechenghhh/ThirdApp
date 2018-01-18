65df926e605bc5eba2adcfe60b8443a3bf53b370
Cheng <lechenggood@foxmail.com>
2018-01-18 10:17:25 +0800

    +监听音量键服务开启 可以 toast 了

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty071VolueEvent.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty071VolueEvent.java
@@ -1,7 +1,5 @@
 package com.lecheng.hello.thirdapp.ActivityList;
 
-import java.util.Calendar;
-
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
@@ -9,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -16,6 +15,11 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.lecheng.hello.thirdapp.R;
+
+import java.util.Calendar;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 //音量键后台监听：http://blog.csdn.net/qq_16564849/article/details/70280755
 //root后模拟按钮：http://blog.csdn.net/jzj1993/article/details/39158865
@@ -29,32 +33,42 @@ public class Aty071VolueEvent extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty071);
+        ButterKnife.bind(this);
         Log.i(TAG, "onCreate");
-        registerBroader();
-        findViewById(R.id.btn_start).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        openServiceSetting();
-                    }
-                });
+        keycodebroadreceiver = new KeycodeBroadReceiver();
+    }
+
+    @OnClick({R.id.btnAccessibility, R.id.btnRegister, R.id.btnUnRegister})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAccessibility:
+                try {
+                    startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+                    Toast.makeText(this, "找到按键监听服务，开启即可", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btnRegister:
+                IntentFilter intentfilter = new IntentFilter();
+                intentfilter.addAction("com.exmaple.broadcaster.KEYDOWN");
+                intentfilter.addAction("com.exmaple.broadcaster.KEYUP");
+                registerReceiver(keycodebroadreceiver, intentfilter);
+                break;
+            case R.id.btnUnRegister:
+                unregisterReceiver(keycodebroadreceiver);
+                break;
+        }
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         int key = event.getKeyCode();
-
         switch (key) {
             case KeyEvent.KEYCODE_VOLUME_DOWN:
                 Time t1 = new Time();
                 t1.setToNow();
-                int year1 = t1.year;
-                int month1 = t1.month;
-                int date1 = t1.monthDay;
-                int hour1 = t1.hour;
-                int minute1 = t1.minute + 1;
-                int second1 = t1.second;
-                c.set(year1, month1, date1, hour1, (int) minute1, second1);
+                c.set(t1.year, t1.month, t1.monthDay, t1.hour, (int) t1.minute + 1, t1.second);
                 AlarmManager am2 = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
                 am2.setTime((c.getTimeInMillis()));
                 Intent downintent = new Intent("com.exmaple.broadcaster.KEYDOWN");
@@ -71,63 +85,38 @@ public class Aty071VolueEvent extends Activity {
             default:
                 break;
         }
-
         return super.onKeyDown(keyCode, event);
     }
 
-    private void registerBroader() {
-        Log.i(TAG, "Aty071VolueEvent::registerBroader");
-        keycodebroadreceiver = new KeycodeBroadReceiver();
-        IntentFilter intentfilter = new IntentFilter();
-        intentfilter.addAction("com.exmaple.broadcaster.KEYDOWN");
-        intentfilter.addAction("com.exmaple.broadcaster.KEYUP");
-        registerReceiver(keycodebroadreceiver, intentfilter);
-    }
 
-    private void openServiceSetting() {
-        try {
-            Intent intent = new Intent(
-                    android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
-            startActivity(intent);
-            Toast.makeText(this, "找到按键监听服务，开启即可", Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Time t = new Time();
-    private Time t1 = new Time();
+    private Time t3 = new Time();
+    private Time t4 = new Time();
 
     private class KeycodeBroadReceiver extends BroadcastReceiver {
         private static final String TAG = "ONRECEIVE";
 
         @Override
         public void onReceive(Context context, Intent intent) {
-
             if (intent.getAction() == "com.exmaple.broadcaster.KEYUP") {
-                t.setToNow();
-                int year = t.year;
-                int month = t.month;
-                int date = t.monthDay;
-                int hour = t.hour;
-                int minute = t.minute + 1;
-                int second = t.second;
+                t3.setToNow();
+                int year = t3.year;
+                int month = t3.month;
+                int date = t3.monthDay;
+                int hour = t3.hour;
+                int minute = t3.minute + 1;
+                int second = t3.second;
                 c.set(year, month, date, hour, minute, second);
-
                 AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
                 am.setTime((c.getTimeInMillis()));
-
                 Log.i(TAG, "KeycodeBroadReceiver::onReceive");
-
             } else if (intent.getAction() == "com.exmaple.broadcaster.KEYDOWN") {
-
-                t1.setToNow();
-                int year1 = t1.year;
-                int month1 = t1.month;
-                int date1 = t1.monthDay;
-                int hour1 = t1.hour;
-                int minute1 = t1.minute - 1;
-                int second1 = t1.second;
+                t4.setToNow();
+                int year1 = t4.year;
+                int month1 = t4.month;
+                int date1 = t4.monthDay;
+                int hour1 = t4.hour;
+                int minute1 = t4.minute - 1;
+                int second1 = t4.second;
                 c.set(year1, month1, date1, hour1, (int) minute1, second1);
                 AlarmManager am2 = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
                 am2.setTime((c.getTimeInMillis()));
--- app/src/main/java/com/lecheng/hello/thirdapp/Service/Serv071VolueEvent.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/Service/Serv071VolueEvent.java
@@ -38,26 +38,28 @@ public class Serv071VolueEvent extends AccessibilityService {
         int key = event.getKeyCode();
         switch (key) {
             case KeyEvent.KEYCODE_VOLUME_DOWN:
-                Intent downintent = new Intent("com.exmaple.broadcaster.KEYDOWN");
-                downintent.putExtra("dtime", System.currentTimeMillis());
-                if (flag == 0) {
-                    sendBroadcast(downintent);
-                } else if (flag == 1) {
-                    flag = 0;
-                }
-                Log.i(TAG, "KEYCODE_VOLUME_DOWN");
+                //发广播
+//                Intent downintent = new Intent("com.exmaple.broadcaster.KEYDOWN");
+//                downintent.putExtra("dtime", System.currentTimeMillis());
+//                if (flag == 0) {
+//                    sendBroadcast(downintent);
+//                } else if (flag == 1) {
+//                    flag = 0;
+//                }
+//                Log.i(TAG, "KEYCODE_VOLUME_DOWN");
                 Toast.makeText(Serv071VolueEvent.this, "音量-被按下", Toast.LENGTH_SHORT).show();
                 break;
             case KeyEvent.KEYCODE_VOLUME_UP:
-                Intent upintent = new Intent("com.exmaple.broadcaster.KEYUP");
-                upintent.putExtra("utime", System.currentTimeMillis());
-                if (flag == 0) {
-                    sendBroadcast(upintent);
-                    flag += 1;
-                } else if (flag == 1) {
-                    flag = 0;
-                }
-                Log.i(TAG, "KEYCODE_VOLUME_UP");
+                //发广播
+//                Intent upintent = new Intent("com.exmaple.broadcaster.KEYUP");
+//                upintent.putExtra("utime", System.currentTimeMillis());
+//                if (flag == 0) {
+//                    sendBroadcast(upintent);
+//                    flag += 1;
+//                } else if (flag == 1) {
+//                    flag = 0;
+//                }
+//                Log.i(TAG, "KEYCODE_VOLUME_UP");
                 Toast.makeText(Serv071VolueEvent.this, "音量+被按下", Toast.LENGTH_SHORT).show();
                 break;
             default:
@@ -84,69 +86,66 @@ public class Serv071VolueEvent extends AccessibilityService {
     }
 
     private void createToucher() {
-        //赋值WindowManager&LayoutParam.
-        params = new WindowManager.LayoutParams();
-        windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
-        //设置type.系统提示型窗口，一般都在应用程序窗口之上.
-        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-        //设置效果为背景透明.
-        params.format = PixelFormat.RGBA_8888;
-        //设置flags.不可聚焦及不可使用按钮对悬浮窗进行操控.
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        if (toucherLayout == null) {
+            //赋值WindowManager&LayoutParam.
+            params = new WindowManager.LayoutParams();
+            windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
+            //设置type.系统提示型窗口，一般都在应用程序窗口之上.
+            params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+            //设置效果为背景透明.
+            params.format = PixelFormat.RGBA_8888;
+            //设置flags.不可聚焦及不可使用按钮对悬浮窗进行操控.
+            params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 
-        //设置窗口初始停靠位置.
-        params.gravity = Gravity.LEFT | Gravity.TOP;
-        params.x = 0;
-        params.y = 0;
+            //设置窗口初始停靠位置.
+            params.gravity = Gravity.LEFT | Gravity.TOP;
+            params.x = 0;
+            params.y = 0;
 
-        //设置悬浮窗口长宽数据.
-        //注意，这里的width和height均使用px而非dp.这里我偷了个懒
-        //如果你想完全对应布局设置，需要先获取到机器的dpi
-        //px与dp的换算为px = dp * (dpi / 160).
-        params.width = 300;
-        params.height = 300;
+            //设置悬浮窗口长宽数据.
+            //注意，这里的width和height均使用px而非dp.这里我偷了个懒
+            //如果你想完全对应布局设置，需要先获取到机器的dpi
+            //px与dp的换算为px = dp * (dpi / 160).
+            params.width = 300;
+            params.height = 300;
 
-        LayoutInflater inflater = LayoutInflater.from(getApplication());
-        //获取浮动窗口视图所在布局.
-        toucherLayout = (LinearLayout) inflater.inflate(R.layout.unit_suspension, null);
-        //添加toucherlayout
-        windowManager.addView(toucherLayout, params);
+            LayoutInflater inflater = LayoutInflater.from(getApplication());
+            //获取浮动窗口视图所在布局.
+            toucherLayout = (LinearLayout) inflater.inflate(R.layout.unit_suspension, null);
+            //添加toucherlayout
+            windowManager.addView(toucherLayout, params);
 
-        Log.i(TAG, "toucherlayout-->left:" + toucherLayout.getLeft());
-        Log.i(TAG, "toucherlayout-->right:" + toucherLayout.getRight());
-        Log.i(TAG, "toucherlayout-->top:" + toucherLayout.getTop());
-        Log.i(TAG, "toucherlayout-->bottom:" + toucherLayout.getBottom());
+            Log.i(TAG, "toucherlayout-->left:" + toucherLayout.getLeft());
+            Log.i(TAG, "toucherlayout-->right:" + toucherLayout.getRight());
+            Log.i(TAG, "toucherlayout-->top:" + toucherLayout.getTop());
+            Log.i(TAG, "toucherlayout-->bottom:" + toucherLayout.getBottom());
 
-        //主动计算出当前View的宽高信息.
-        toucherLayout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            //主动计算出当前View的宽高信息.
+            toucherLayout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
 
-        //用于检测状态栏高度.
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
-        }
-        Log.i(TAG, "状态栏高度为:" + statusBarHeight);
-
-        //浮动窗口按钮.
-        ivBtn = (ImageButton) toucherLayout.findViewById(R.id.imageButton1);
-        ivBtn.setBackgroundResource(R.drawable.ic_play);
-        //其他代码...
-//        ivBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(Serv071VolueEvent.this, "点击了悬浮窗", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-        ivBtn.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                //ImageButton我放在了布局中心，布局一共300dp
-                params.x = (int) event.getRawX() - 150;
-                //这就是状态栏偏移量用的地方
-                params.y = (int) event.getRawY() - 150 - statusBarHeight;
-                windowManager.updateViewLayout(toucherLayout, params);
-                return false;
+            //用于检测状态栏高度.
+            int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                statusBarHeight = getResources().getDimensionPixelSize(resourceId);
             }
-        });
+            Log.i(TAG, "状态栏高度为:" + statusBarHeight);
+
+            //浮动窗口按钮.
+            ivBtn = (ImageButton) toucherLayout.findViewById(R.id.imageButton1);
+            ivBtn.setBackgroundResource(R.drawable.ic_play);
+            //其他代码...
+
+            ivBtn.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    //ImageButton我放在了布局中心，布局一共300dp
+                    params.x = (int) event.getRawX() - 150;
+                    //这就是状态栏偏移量用的地方
+                    params.y = (int) event.getRawY() - 150 - statusBarHeight;
+                    windowManager.updateViewLayout(toucherLayout, params);
+                    return false;
+                }
+            });
+        }
     }
 }
