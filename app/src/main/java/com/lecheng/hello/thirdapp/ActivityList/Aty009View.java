1231886053e392f8a56d9fb05514d8f85f5ea374
lechenghhh <lechenggood@foxmail.com>
2018-06-06 11:05:56 +0800

    添加获取签名功能

--- app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty009View.java
+++ app/src/main/java/com/lecheng/hello/thirdapp/ActivityList/Aty009View.java
@@ -1,13 +1,21 @@
 package com.lecheng.hello.thirdapp.ActivityList;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import com.lecheng.hello.thirdapp.R;
 import com.lecheng.hello.thirdapp.Widgets.RippleBackground;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,6 +33,20 @@ public class Aty009View extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty009);
         ButterKnife.bind(this);
+        try {//获取hashkey
+            int i = 0;
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                i++;
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String KeyHash = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                EditText edid = (EditText) findViewById(R.id.etid);
+                edid.setText(KeyHash);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
     }
 
 
